de23814297b64b6d242ed4a34296ad27ac575515
Nguyễn Quốc Huy <ongdo2004@gmail.com>
2018-07-05 23:35:50 +0700

    #update: - hỗ trợ hiệu ứng fadein cho các news-card;

--- trunk/news-web/src/app/ui-component/news-card/news-card.component.ts
+++ trunk/news-web/src/app/ui-component/news-card/news-card.component.ts
@@ -1,15 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {NewsView} from "../../bean/news-view";
 import {Image} from "../../bean/image";
 import {Router} from "@angular/router";
 import {ApplicationUtils} from "../../common/application-utils";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-news-card',
     templateUrl: './news-card.component.html',
-    styleUrls: ['./news-card.component.css']
+    styleUrls: ['./news-card.component.css'],
+    animations: [
+        trigger("readyStateChanged", [
+            state('yes', style({opacity: 0})),
+            state('no', style({opacity: 1})),
+            transition('* => *', animate('0.5s'))
+        ])
+    ]
 })
-export class NewsCardComponent implements OnInit {
+export class NewsCardComponent implements OnInit, AfterViewInit {
 
     @Input()
     news: NewsView;
@@ -19,6 +27,8 @@ export class NewsCardComponent implements OnInit {
 
     @Input()
     now: Date;
+
+    readyState: string = "no";
 
     constructor(protected router: Router,
                 protected applicationUtils: ApplicationUtils) {
@@ -34,5 +44,10 @@ export class NewsCardComponent implements OnInit {
         let title: string = this.applicationUtils.convertTitleToURLParam(this.news.title);
 
         this.router.navigate(['/newsDetail/', title, this.news.id]).then();
+    }
+
+    ngAfterViewInit(): void {
+
+        this.readyState = "yes";
     }
 }
