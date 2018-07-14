010f013de7ca8b815538f1e7d9cc90679f6eb1ae
Nguyễn Quốc Huy <ongdo2004@gmail.com>
2018-07-14 12:02:24 +0700

    #update: - thêm phần render categories;

--- trunk/news-web/src/app/service/category-fixed-datasource.ts
+++ trunk/news-web/src/app/service/category-fixed-datasource.ts
@@ -2,22 +2,463 @@ import {Category} from "../bean/category";
 
 export const ALL_CATEGORIES: Category[] = [
     {
-        "id": "81a9b479e9434be6abef5c99ab669816",
+        "id": "02db81b6-94c5-415d-8a80-9c54649da741",
         "isDeleted": false,
-        "code": "TinChinh",
+        "code": "KhieuNai",
         "lastModifiedUser": "admin",
-        "title": "Tin Chính",
+        "title": "Khiếu nại",
         "parentCategoryId": null,
-        "lastModifiedTime": "2018-06-25T09:29:07Z"
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
     },
     {
-        "id": "c00ac37ecfd146f38ffba7399ce87181",
+        "id": "0b32c3b8-b53c-4f3c-a6d6-719c3fefa168",
+        "isDeleted": false,
+        "code": "DiaOc",
+        "lastModifiedUser": "admin",
+        "title": "Địa ốc",
+        "parentCategoryId": "02db81b6-94c5-415d-8a80-9c54649da741",
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "12f577d2-557b-405b-a418-0610cbded825",
         "isDeleted": false,
         "code": "ThoiSu",
         "lastModifiedUser": "admin",
-        "title": "Thời Sự",
+        "title": "Thời sự",
         "parentCategoryId": null,
-        "lastModifiedTime": "2018-06-25T09:29:07Z"
-    }
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "159bf50c-40f8-43d2-91b2-4f763ce97eec",
+        "isDeleted": false,
+        "code": "ChiemTinh",
+        "lastModifiedUser": "admin",
+        "title": "Chiêm tinh",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "1bc09a1e-d8d4-4b01-b081-cf14b1463444",
+        "isDeleted": false,
+        "code": "VanHoa",
+        "lastModifiedUser": "admin",
+        "title": "Văn hóa",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "1ebd44cb-1ad5-4597-858a-978882c57765",
+        "isDeleted": false,
+        "code": "TaiChinh",
+        "lastModifiedUser": "admin",
+        "title": "Tài chính",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "23b0f84f-3828-4a4c-aec2-6501d2ec309a",
+        "isDeleted": false,
+        "code": "TheGioi",
+        "lastModifiedUser": "admin",
+        "title": "Thế giới",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "2556fab6-1139-4dd7-bc52-c124aebbc40d",
+        "isDeleted": false,
+        "code": "GiaoThong",
+        "lastModifiedUser": "admin",
+        "title": "Giao thông",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "2fb4c139-9e35-4eaa-9660-8422fbf6db72",
+        "isDeleted": false,
+        "code": "DoiSong",
+        "lastModifiedUser": "admin",
+        "title": "Đời sống",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    {
+        "id": "31b188a8-74f4-4d41-8124-e71d78252768",
+        "isDeleted": false,
+        "code": "TapChi",
+        "lastModifiedUser": "admin",
+        "title": "Tạp chí",
+        "parentCategoryId": null,
+        "lastModifiedTime": "2018-06-27T07:08:02Z"
+    },
+    // {
+    //     "id": "4253af44-64af-4605-84c2-8d7f389c3eac",
+    //     "isDeleted": false,
+    //     "code": "GocNhin",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Góc nhìn",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "4fdf5361-3544-4b37-a1cc-6ac44982fc51",
+    //     "isDeleted": false,
+    //     "code": "DuLich",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Du lịch",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "54ea0f12-f371-4fbb-ad14-a4bd0c36bb54",
+    //     "isDeleted": false,
+    //     "code": "HocSinhSinhVien",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Học sinh Sinh viên",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "571c8fc2-f021-4f2b-8fc2-49b764998f87",
+    //     "isDeleted": false,
+    //     "code": "SucKhoe",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Sức khỏe",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "57eda84c-1bb3-4558-9b79-4a1f5cf7d629",
+    //     "isDeleted": false,
+    //     "code": "CanhBao",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Cảnh báo",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "5e483401-2466-45b2-a521-bbf22ea7bde2",
+    //     "isDeleted": false,
+    //     "code": "MoiTruong",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Môi trường",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "6050fc8f-9d17-4960-bf5d-4aa652501b82",
+    //     "isDeleted": false,
+    //     "code": "Video",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Video",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "6a494fbc-0f09-4ab0-95a3-12c1b7e02945",
+    //     "isDeleted": false,
+    //     "code": "XaHoi",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Xã hội",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "6e93de2b-09bf-4e1b-a804-ea0b877dfb01",
+    //     "isDeleted": false,
+    //     "code": "ChinhTri",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Chính trị",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "70da8161-7f75-474f-acdf-6b76e825d96c",
+    //     "isDeleted": false,
+    //     "code": "PhapLuat",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Giáo dục",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "727e5ca6-ef74-43d6-b000-e51c402a7306",
+    //     "isDeleted": false,
+    //     "code": "BinhChonSanPham",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Bình chọn sản phẩm",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "7f15cbf2-0619-4546-be94-4f66a7fa9a3d",
+    //     "isDeleted": false,
+    //     "code": "KhamPha",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Khám phá",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "82483751-ff0c-44cf-88aa-3fbde77d2040",
+    //     "isDeleted": false,
+    //     "code": "CongNghe",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Công nghệ ",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "894655cb-e709-418a-b8c8-082dae538de1",
+    //     "isDeleted": false,
+    //     "code": "NguoiTieuDung",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Người Tiêu dùng",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "90916f71-d10d-4540-b5ac-a7f857721b96",
+    //     "isDeleted": false,
+    //     "code": "KhoaHoc",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Khoa học",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "978144c6-bea2-42ed-abb7-f1b407eb5a70",
+    //     "isDeleted": false,
+    //     "code": "TrangChu",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Trang chủ",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "9f428bcb-478a-4826-8439-4733c96f4e45",
+    //     "isDeleted": false,
+    //     "code": "SuKienBinhLuan",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Sự kiện - Bình luận",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "a0ae0bad-02a2-47b4-b13e-4281e7cd55e1",
+    //     "isDeleted": false,
+    //     "code": "ThuGian",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Thư giãn",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "adf64bf0-c244-4ade-8a99-4f639f56bce7",
+    //     "isDeleted": false,
+    //     "code": "GioiTinh",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Giới tính",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "af66aeeb-b351-4260-a682-8de551b15d05",
+    //     "isDeleted": false,
+    //     "code": "TinNong",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Tin nóng",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "afd4d691-d7c5-45b1-970d-d1f484895924",
+    //     "isDeleted": false,
+    //     "code": "AnNinhTratTu",
+    //     "lastModifiedUser": "admin",
+    //     "title": "An ninh trật tự",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "aff14f25-a36c-42f7-9f69-0cb414c5b683",
+    //     "isDeleted": false,
+    //     "code": "NhipSong",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Nhịp Sống",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "b1165584-8c50-4960-a83c-3e19f07f8a76",
+    //     "isDeleted": false,
+    //     "code": "Anh",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Ảnh",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "b9664234-cc43-414a-a64e-53fd5f1ceb3d",
+    //     "isDeleted": false,
+    //     "code": "NguoiViet",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Người Việt",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "bae0d5ee-a8dd-45df-8bee-7a121c10f706",
+    //     "isDeleted": false,
+    //     "code": "TruyenHinhTV",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Truyền Hình - TV",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "bfec4714-3824-43a1-9ee1-17efbd099c26",
+    //     "isDeleted": false,
+    //     "code": "TinNoiBat",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Tin nổi bật",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "c0e3235c-424e-4cbb-acfa-471f18671c8d",
+    //     "isDeleted": false,
+    //     "code": "Xe",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Xe ",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "c238a1ce-fb25-44bd-869d-e75c614c1b7a",
+    //     "isDeleted": false,
+    //     "code": "Thien",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Thiền",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "c47976bc-3860-49ef-8b82-6fe776f491df",
+    //     "isDeleted": false,
+    //     "code": "CongDoan",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Công đoàn",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "c548902c-9338-4b93-aac1-f25aa252e935",
+    //     "isDeleted": false,
+    //     "code": "TheThao",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Thể thao ",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "cf48e435-f893-4492-826d-9d462f7b6648",
+    //     "isDeleted": false,
+    //     "code": "GocNhinDaChieu",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Góc nhìn đa chiều",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "d350f61e-f130-4e78-9cbf-7b29cc1c810c",
+    //     "isDeleted": false,
+    //     "code": "ToiViet",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Tôi viết",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "d6cc2752-5d45-43ca-860e-a9c675e1fd2d",
+    //     "isDeleted": false,
+    //     "code": "GiaiTri",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Giải trí",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "d959b0f2-1664-4f22-b06c-b48321808e3c",
+    //     "isDeleted": false,
+    //     "code": "Game",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Game",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "dce8c81f-1b5a-487e-9e72-e7052a7796d7",
+    //     "isDeleted": false,
+    //     "code": "KhoTriThuc",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Kho tri thức",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "ddea32a7-2d6c-43ed-bddc-ae4cbfeffec3",
+    //     "isDeleted": false,
+    //     "code": "ViecLam",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Việc làm",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "eb078756-1a65-415d-8bfa-33f07d5658b4",
+    //     "isDeleted": false,
+    //     "code": "KetNoi",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Kết nối",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "f02f6f65-e9c1-437d-8c13-c5a0a9ac314e",
+    //     "isDeleted": false,
+    //     "code": "GioiTre",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Giới trẻ",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "f0b2ebb7-83ab-4087-ab9a-9ec38d14bfc8",
+    //     "isDeleted": false,
+    //     "code": "PhapLuat",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Pháp luật",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "f523a69b-d855-4dca-bcf9-fad33ff12410",
+    //     "isDeleted": false,
+    //     "code": "HoaHau",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Hoa hậu",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // },
+    // {
+    //     "id": "f766b734-2141-452f-b509-3b016476cf30",
+    //     "isDeleted": false,
+    //     "code": "DienDan",
+    //     "lastModifiedUser": "admin",
+    //     "title": "Diễn đàn",
+    //     "parentCategoryId": null,
+    //     "lastModifiedTime": "2018-06-27T07:08:02Z"
+    // }
 ];
 
